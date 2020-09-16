da744cddc10168d6bbcb989d573947854c74eb2c
raimaaan03 <43918793+raimaaan03@users.noreply.github.com>
2020-09-16 18:11:20 -0300

    firefox port
    yes that's literally it, a single class name change

--- hana.js
+++ hana.js
@@ -1,9 +1,9 @@
 var hana = document.createElement('img');
-hana.src = chrome.runtime.getURL("assets/hana.png");
+hana.src = browser.runtime.getURL("assets/hana.png");
 hana.style.cssText = "z-index: 100; position: fixed; right: -50%; bottom: 0px; height: 80%; transition-property: right; transition-timing-function: cubic-bezier(.18,.85,.15,.98); transition-duration: 3s;"
 document.body.appendChild(hana);
 
-var audio = new Audio(chrome.runtime.getURL("assets/hana.mp3"));
+var audio = new Audio(browser.runtime.getURL("assets/hana.mp3"));
 var played = false;
 document.addEventListener("click", function(){
 	if (!played) {
